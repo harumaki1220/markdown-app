15b66cd936670c9d68b897261323e90fe112abe5
harumaki1220 <haruma181220@gmail.com>
2025-12-11 18:48:00 +0900

    feat: ダークモード対応

--- src/App.tsx
+++ src/App.tsx
@@ -8,14 +8,14 @@ function App() {
   const [markdown, setMarkdown] = useState<string>("");
 
   return (
-    <div className="flex h-screen">
+    <div className="flex h-screen bg-gray-900 text-white">
       <textarea
-        className="w-1/2 prose p-4"
+        className="w-1/2 prose prose-invert p-4 bg-transparent resize-none outline-none"
         placeholder="マークダウンを入力してください"
         value={markdown}
         onChange={(e) => setMarkdown(e.target.value)}
       />
-      <div className="w-1/2 prose p-4">
+      <div className="w-1/2 prose prose-invert p-4">
         <ReactMarkdown remarkPlugins={[remarkGfm]}>{markdown}</ReactMarkdown>
       </div>
     </div>
